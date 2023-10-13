34521e04cf91f41e8bcff00fdd26f86a8e469e26
Bhaskar rai <bhaskarrai2891@gmail.com>
2023-10-13 23:55:15 +0530

    Create percentage.py

---
+++ percentage.py
@@ -0,0 +1,14 @@
+# Get three numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Perform addition, multiplication, and subtraction
+addition_result = num1 + num2 + num3
+multiplication_result = num1 * num2 * num3
+subtraction_result = num1 - num2 - num3
+
+# Display the results
+print(f"Addition Result: {addition_result}")
+print(f"Multiplication Result: {multiplication_result}")
+print(f"Subtraction Result: {subtraction_result}")
